811cfc5f41627034fdec87586163fbf9210166a2
Marcus Hitchins <Marcus7777@gmail.com>
2015-09-12 12:56:40 +0100

    espeak

--- go.sh
+++ go.sh
@@ -1,1 +1,2 @@
-git pull && bower update && gulp && rsync --exclude .htaccess -a ./dist/ root@open-elements.org:/var/www/html/open-elements.org/
+git pull && bower update && gulp && rsync --exclude .htaccess -a ./dist/ root@open-elements.org:/var/www/html/open-elements.org/ && espeak "updated open elements"
+beep
