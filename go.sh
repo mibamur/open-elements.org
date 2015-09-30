1996ee5f46f3456afbdf28a6de6f9a47dbb1fa12
Marcus Hitchins <marcus7777@gmail.com>
2015-09-30 11:22:33 +0100

    list of tests

--- go.sh
+++ go.sh
@@ -1,2 +1,2 @@
-git pull && bower update && gulp && rsync --exclude .htaccess -a ./dist/ root@open-elements.org:/var/www/html/open-elements.org/ && espeak "updated open elements"
+git pull && bower update && gulp && ls -d bower_components/*/test/ > dist/list-of-tests && ls bower_components/*/*.html > dist/list-of-elements && rsync --exclude .htaccess -a ./dist/ root@open-elements.org:/var/www/html/open-elements.org/ && espeak "updated open elements"
 beep
